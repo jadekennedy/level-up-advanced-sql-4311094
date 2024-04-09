7bd937c8a6279533fad5e654defa4b0847935602
Jade Kennedy <100635254+jadekennedy@users.noreply.github.com>
2024-04-09 14:53:56 +0000

    Exercise 2

--- Scripts/Advanced Challenges.sql
+++ Scripts/Advanced Challenges.sql
@@ -1,5 +1,18 @@
+-- Exercise 1: Return Employees and List of Managers --
+-- SELF JOIN --
+
 SELECT a.firstName 'Employee First Name', a.lastName 'Employee Last Name', 
   b.firstName 'Manager First Name', b.lastName 'Manager Last Name'
 FROM employee a
 JOIN employee b
   ON a.managerId = b.employeeId;
+
+
+-- Exercise 2: Find Salespersons with 0 Sales --
+-- LEFT JOIN TO IDENTIFY NULLS --
+
+SELECT e.firstName 'First Name', e.lastName 'Last Name' 
+FROM employee e 
+LEFT JOIN sales s
+  ON e.employeeId = s.employeeId
+WHERE s.employeeId IS NULL;
